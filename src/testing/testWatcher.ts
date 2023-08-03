3114dd7de68d700e75a99f9dc3569e1fe86108db
Paul Sauer <paulrsauer@gmail.com>
2023-08-03 01:22:50 -0400

    no more test

--- src/testing/testWatcher.ts
+++
@@ -1,9 +0,0 @@
-import { R3DLogWatcher } from "./logWatcher";
-
-const watcher = new R3DLogWatcher(
-  "C:\\Riot Games\\League of Legends\\Logs\\GameLogs\\TEST\\"
-);
-
-watcher.on("new_line", (newline) => {
-  console.log(newline);
-});
