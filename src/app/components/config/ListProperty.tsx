a93780089631af8fead13467faeb65722f82d7bb
Paul Sauer <paulrsauer@gmail.com>
2025-07-31 12:50:47 -0400

    bugfix

--- src/app/components/config/ListProperty.tsx
+++ src/app/components/config/ListProperty.tsx
@@ -16,7 +16,7 @@ export const ListProperty = ({ property, value, onChange }: PropertyInputProps) 
     <div className="mb-4">
       <label className="block text-gray-100 mb-1">{property.description}</label>
       <select
-        value={value || '-1'}
+        value={value === undefined ? -1 : value}
         onChange={(e) => onChange(property.name, e.target.value)}
         disabled={!property.enabled}
         className="bg-gray-800 text-gray-100 px-3 py-1 rounded border border-gray-600 focus:border-green-500 w-full"
